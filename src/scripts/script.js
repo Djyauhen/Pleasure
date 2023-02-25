9128e4ac4489deb388fab2cb5d3149222282879f
Yauheni Fursa <fursaevgenij@gmail.com>
2023-02-26 00:06:28 +0300

    add popups

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,6 +1,9 @@
 let popups = $('.popups');
 let popupRafaello = $('.popup-rafaello');
+let popupOrange = $('.popup-orange');
 let rafaello = $('.rafaello');
+let orange = $('.orange');
+let close = $('.close');
 
 rafaello.click(function () {
     popups.css({
@@ -9,11 +12,31 @@ rafaello.click(function () {
     popupRafaello.css({
         display: 'flex'
     });
-
 });
 
-popups.click(function () {
+close.click(function () {
     popups.css({
+        display: 'none'
+    });
+    popupRafaello.css({
+        display: 'none'
+    });
+});
+
+orange.click(function () {
+    popups.css({
+        display: 'flex'
+    });
+    popupOrange.css({
+        display: 'flex'
+    });
+});
+
+close.click(function () {
+    popups.css({
+        display: 'none'
+    });
+    popupOrange.css({
         display: 'none'
     });
 });
